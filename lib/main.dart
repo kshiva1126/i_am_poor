bca979587d4a1822806f2a39d73c6caf6cacb1cf
kshiva1126 <42695879+kshiva1126@users.noreply.github.com>
2020-09-07 23:16:54 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Image(
+            image: AssetImage('images/tasyumi_kiyoubinbou.png'),
+          ),
+        ),
+        appBar: AppBar(
+          title: Text(
+            'I am poor',
+          ),
+          backgroundColor: Colors.lightBlue,
+        ),
+      ),
+    );
+  }
+}
